825bce61f2bb4104abd9e8279ee48fb58bfab49a
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2023-02-17 19:04:15 +0530

    4.2

--- 4.2/pages/locators.py
+++ 4.2/pages/locators.py
@@ -1,12 +1,12 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
-
-
-class  LoginPageLocators():
-    register_form = (By.CSS_SELECTOR, "button[value='Register']")
-    login_form = (By.CSS_SELECTOR, "button[value='Log In']")
+# from selenium.webdriver.common.by import By
+#
+#
+# class MainPageLocators():
+#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+#
+#
+#
+#
+# class  LoginPageLocators():
+#     register_form = (By.CSS_SELECTOR, "button[value='Register']")
+#     login_form = (By.CSS_SELECTOR, "button[value='Log In']")
--- 4.2/test_main_page.py
+++ 4.2/test_main_page.py
@@ -30,5 +30,20 @@ def test_guest_can_see_login_page(browser):
     page.should_be_login_form()
     page.should_be_login_url()
 
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com"
+#     page = MainPage(browser, link)
+#     page.open()
+#     login_page = page.go_to_login_page()
+#     login_page.should_be_login_page()
 
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
